2d75f12c4d21df5052e384d0be2985660d4f4bd6
prachi602 <ps1429@srmist.edu.in>
2026-03-12 00:58:00 +0530

    Update ArraysLoops.java

--- ArraysLoops.java
+++ ArraysLoops.java
@@ -8,13 +8,13 @@ public class ArraysLoops {
         String lines[] = new String[7];
 
         // Populate each line using String.join()
-        lines[0] = String.join("  ", "  ***  ", "******", "******", "  ****");
-        lines[1] = String.join("  ", "**   **", "**  **", "**  **", "**    ");
-        lines[2] = String.join("  ", "**   **", "**  **", "**  **", "**    ");
-        lines[3] = String.join("  ", "**   **", "******", "******", "******");
-        lines[4] = String.join("  ", "**   **", "**    ", "**    ", "    **");
-        lines[5] = String.join("  ", "**   **", "**    ", "**    ", "    **");
-        lines[6] = String.join("  ", "  ***  ", "**    ", "**    ", "******");
+        lines[0] = String.join("  ", "  ***  ", "  ***  ", "******", "  ****");
+        lines[1] = String.join("  ", "**   **", "**   **", "**  **", "**    ");
+        lines[2] = String.join("  ", "**   **", "**   **", "**  **", "**    ");
+        lines[3] = String.join("  ", "**   **", "**   **", "******", "******");
+        lines[4] = String.join("  ", "**   **", "**   **", "**    ", "    **");
+        lines[5] = String.join("  ", "**   **", "**   **", "**    ", "    **");
+        lines[6] = String.join("  ", "  ***  ", "  ***  ", "**    ", "****  ");
 
         // Print each line using a for-each loop
         for (String line : lines) {
