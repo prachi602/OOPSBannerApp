376f1703315922a26bf57b4e455638b098dca988
prachi602 <ps1429@srmist.edu.in>
2026-03-11 23:53:52 +0530

    UC2- OOPS Banner Printed

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+// OOPS Banner App
+// Each character is 7 lines tall, 9 characters wide
+// Printed using only System.out.println()
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("  ***  "+"  "+"******"+"  "+"******"+"  "+"  ****");
+        System.out.println("**   **"+"  "+"**  **"+"  "+"**  **"+"  "+"**    ");
+        System.out.println("**   **"+"  "+"**  **"+"  "+"**  **"+"  "+"**    ");
+        System.out.println("**   **"+"  "+"******"+"  "+"******"+"  "+"******");
+        System.out.println("**   **"+"  "+"**    "+"  "+"**    "+"  "+"    **");
+        System.out.println("**   **"+"  "+"**    "+"  "+"**    "+"  "+"    **");
+        System.out.println("  ***  "+"  "+"**    "+"  "+"**    "+"  "+"******");
+    }
+}
